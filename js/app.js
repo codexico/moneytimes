2b47c018c2b93fb5dd1ae961d219743ed00b95fc
Francisco Kahil <codexico@gmail.com>
2017-05-22 21:07:43 -0300

    Dont open all on blank anymore

--- js/app.js
+++ js/app.js
@@ -78,16 +78,4 @@
     }
     insertPatrocinadoBetweenNews();
 
-    function addBlankTolinks(el) {
-        el.querySelectorAll('a:not([target="_blank"]):not(.site-logo)')
-            .forEach(function(link) {
-                link.setAttribute('target', '_blank');
-            });
-    }
-    // Drupal.behaviors.targetBlank = {
-    //     attach: function (context) {
-    //         addBlankTolinks(context);
-    //     }
-    // };
-
 })();
---
+++ js/blank.js
@@ -0,0 +1,15 @@
+(function() {
+
+    function addBlankTolinks(el) {
+        el.querySelectorAll('a:not([target="_blank"]):not(.site-logo)')
+            .forEach(function(link) {
+                link.setAttribute('target', '_blank');
+            });
+    }
+    Drupal.behaviors.targetBlank = {
+        attach: function (context) {
+            addBlankTolinks(context);
+        }
+    };
+
+})();
