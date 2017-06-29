868de9b6b77881ccac09959e3d54f710bc6e65c7
Francisco Kahil <codexico@gmail.com>
2017-06-28 22:40:42 -0300

    dont show fixed newsletter

--- js/giro.js
+++ js/giro.js
@@ -1,82 +1,87 @@
-(function() {
-    var giro = document.getElementById('block-mailchimpsubscriptionformrecebaanalisesgratis');
+(function initGiro() {
+  'use strict';
+
+  var giro = document.getElementById('block-mailchimpsubscriptionformrecebaanalisesgratis');
 
     // fix placeholder
-    var giroInput = giro.querySelector('.form-email');
-    giroInput.placeholder = 'Digite seu e-mail';
+  var giroInput = giro.querySelector('.form-email');
+  giroInput.placeholder = 'Digite seu e-mail';
 
     // clone giro block
-    var giroClone = giro.cloneNode(true);
+  var giroClone = giro.cloneNode(true);
 
     // adapt form
-    var legend = '<span class="block-mailchimp-signup-legend">Receba notícias e análises com o <b>Giro Money Times</b>! Deixe o seu-email e pronto!</span>'
-    var giroForm = giroClone.querySelector('form');
-    giroForm.childNodes[0].textContent = '';
-    giroForm.insertAdjacentHTML('beforebegin', legend);
+  var legend = '<span class="block-mailchimp-signup-legend">Receba notícias e análises com o <b>Giro Money Times</b>! Deixe o seu-email e pronto!</span>';
+  var giroForm = giroClone.querySelector('form');
+  giroForm.childNodes[0].textContent = '';
+  giroForm.insertAdjacentHTML('beforebegin', legend);
 
     // find elements to insert into
-    var ingiro = document.querySelectorAll('.js-insert-giro');
+  var ingiro = document.querySelectorAll('.js-insert-giro');
 
     // insert giro into
-    for (var i = 0; i < ingiro.length; i++) {
-        ingiro[i].innerHTML = giroClone.outerHTML;
-    }
+  for (var i = 0; i < ingiro.length; i++) {
+    ingiro[i].innerHTML = giroClone.outerHTML;
+  }
 
-    function deFixFooterNewsletterFormPosition(el, $closeIcon) {
-        el.classList.remove('fixed');
-        el.parentNode.classList.remove('mailchimp-parent--fixed');
-        $closeIcon.remove();
-    }
+  function deFixFooterNewsletterFormPosition(el) {
+    el.classList.remove('fixed');
+    el.parentNode.classList.remove('mailchimp-parent--fixed');
+    el.querySelector('.fa-close').remove();
+  }
 
-    function showFixedNewsletterForm(el, newsletterInput, $closeIcon) {
+  function showFixedNewsletterForm(el, newsletterInput) {
         // show
-        window.setTimeout(function () {
-            el.classList.add('show');
-        }, 3500);
+    window.setTimeout(function () {
+      el.classList.add('show');
+    }, 3500);
         // return to original position
-        window.setTimeout(function () {
+    window.setTimeout(function () {
             // if input empty
-            if (!newsletterInput.value) {
-                deFixFooterNewsletterFormPosition(el, $closeIcon);
-            }
-        }, 25000);
-    }
+      if (!newsletterInput.value) {
+        deFixFooterNewsletterFormPosition(el);
+      }
+    }, 25000);
+  }
 
-    function initFooterNewsletterForm() {
-        var el = document.getElementById('block-mailchimpsubscriptionformgiromoneytimes');
+  function addCloseIcon(el, input) {
+    var $closeIcon = jQuery('<i class="fa fa-close"></i>');
+    $closeIcon.css({position: 'absolute', top: '1rem', right: '1rem'});
+    jQuery(el).append($closeIcon);
+
+    $closeIcon.on('click', function () {
+      if (!input.value) {
+        deFixFooterNewsletterFormPosition(el);
+      }
+    });
+  }
+
+  function initFooterNewsletterForm() {
+    var el = document.getElementById('block-mailchimpsubscriptionformgiromoneytimes');
         // remove text messing with layout
-        var newsletterForm = {};
-        var newsletterInput = giro.querySelector('.form-email');
-        var $closeIcon = jQuery('<i class="fa fa-close"></i>');
-        $closeIcon.css({position:'absolute', top:'1rem', right:'1rem'});
+    var newsletterForm = {};
+    var newsletterInput = giro.querySelector('.form-email');
 
         // customize mailchimp form
-        if (el) {
-            el.classList.add('fixed');
-            el.parentNode.classList.add('mailchimp-parent--fixed');
+    if (el) {
+      // addCloseIcon(el, newsletterInput);
 
-            newsletterForm = el.getElementsByTagName('form');
-            newsletterForm[0].firstChild.textContent = '';
-            newsletterInput = el.querySelector('.form-email');
-            newsletterInput.placeholder = 'Digite seu e-mail';
+      el.classList.add('fixed');
+      el.parentNode.classList.add('mailchimp-parent--fixed');
 
+      newsletterForm = el.getElementsByTagName('form');
+      newsletterForm[0].firstChild.textContent = '';
+      newsletterInput = el.querySelector('.form-email');
+      newsletterInput.placeholder = 'Digite seu e-mail';
 
-            jQuery(el).append($closeIcon);
-
-            $closeIcon.on('click', function () {
-                if (!newsletterInput.value) {
-                    deFixFooterNewsletterFormPosition(el, $closeIcon);
-                }
-            });
-
-            if (window.location.pathname === '/') {
-                showFixedNewsletterForm(el, newsletterInput, $closeIcon);
-            } else {
-                el.classList.add('show');
-                deFixFooterNewsletterFormPosition(el, $closeIcon);
-            }
-        }
+      // if (window.location.pathname === '/') {
+      //   showFixedNewsletterForm(el, newsletterInput);
+      // } else {
+        el.classList.add('show');
+        deFixFooterNewsletterFormPosition(el);
+      // }
     }
+  }
 
-    initFooterNewsletterForm();
+  initFooterNewsletterForm();
 })();
